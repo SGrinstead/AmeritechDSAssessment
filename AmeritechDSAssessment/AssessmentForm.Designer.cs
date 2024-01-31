73d66852e0d68613bdc7a2090a284bd5fddf7a5f
SGrinstead <seth.grinstead3299@gmail.com>
2024-01-31 13:53:03 -0800

    select and read file

--- AmeritechDSAssessment/AssessmentForm.Designer.cs
+++ AmeritechDSAssessment/AssessmentForm.Designer.cs
@@ -29,12 +29,13 @@
 		private void InitializeComponent()
 		{
 			this.SelectFileButton = new System.Windows.Forms.Button();
-			this.SelectedFileNameText = new System.Windows.Forms.TextBox();
+			this.SelectedFileNameTextbox = new System.Windows.Forms.TextBox();
+			this.SolveButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// SelectFileButton
 			// 
-			this.SelectFileButton.Location = new System.Drawing.Point(268, 94);
+			this.SelectFileButton.Location = new System.Drawing.Point(35, 43);
 			this.SelectFileButton.Name = "SelectFileButton";
 			this.SelectFileButton.Size = new System.Drawing.Size(75, 23);
 			this.SelectFileButton.TabIndex = 0;
@@ -42,20 +43,31 @@
 			this.SelectFileButton.UseVisualStyleBackColor = true;
 			this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
 			// 
-			// SelectedFileNameText
+			// SelectedFileNameTextbox
 			// 
-			this.SelectedFileNameText.Location = new System.Drawing.Point(268, 147);
-			this.SelectedFileNameText.Name = "SelectedFileNameText";
-			this.SelectedFileNameText.ReadOnly = true;
-			this.SelectedFileNameText.Size = new System.Drawing.Size(100, 20);
-			this.SelectedFileNameText.TabIndex = 1;
+			this.SelectedFileNameTextbox.Location = new System.Drawing.Point(35, 72);
+			this.SelectedFileNameTextbox.Name = "SelectedFileNameTextbox";
+			this.SelectedFileNameTextbox.ReadOnly = true;
+			this.SelectedFileNameTextbox.Size = new System.Drawing.Size(607, 20);
+			this.SelectedFileNameTextbox.TabIndex = 1;
+			// 
+			// SolveButton
+			// 
+			this.SolveButton.Location = new System.Drawing.Point(35, 207);
+			this.SolveButton.Name = "SolveButton";
+			this.SolveButton.Size = new System.Drawing.Size(75, 23);
+			this.SolveButton.TabIndex = 2;
+			this.SolveButton.Text = "Solve";
+			this.SolveButton.UseVisualStyleBackColor = true;
+			this.SolveButton.Click += new System.EventHandler(this.SolveButton_Click);
 			// 
 			// AssessmentForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.SelectedFileNameText);
+			this.Controls.Add(this.SolveButton);
+			this.Controls.Add(this.SelectedFileNameTextbox);
 			this.Controls.Add(this.SelectFileButton);
 			this.Name = "AssessmentForm";
 			this.Text = "Form1";
@@ -67,7 +79,8 @@
 		#endregion
 
 		private System.Windows.Forms.Button SelectFileButton;
-		private System.Windows.Forms.TextBox SelectedFileNameText;
+		private System.Windows.Forms.TextBox SelectedFileNameTextbox;
+		private System.Windows.Forms.Button SolveButton;
 	}
 }
 
--- AmeritechDSAssessment/AssessmentForm.cs
+++ AmeritechDSAssessment/AssessmentForm.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +14,40 @@ namespace AmeritechDSAssessment
 {
 	public partial class AssessmentForm : Form
 	{
+        private string FilePath { get; set; }
+		private string[] FileLines { get; set; }
+
 		public AssessmentForm()
 		{
 			InitializeComponent();
+			FilePath = string.Empty;
+			
 		}
 
 		private void SelectFileButton_Click(object sender, EventArgs e)
 		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+			DialogResult result = openFileDialog.ShowDialog();
 
+			if(result == DialogResult.OK)
+			{
+				FilePath = openFileDialog.FileName;
+				SelectedFileNameTextbox.Text = FilePath;
+			}
 		}
+
+		private void SolveButton_Click(object sender, EventArgs e)
+		{
+			if(FilePath != string.Empty)
+			{
+				FileLines = File.ReadAllLines(FilePath);
+				foreach(string s in FileLines)
+				{
+					Debug.WriteLine(s);
+				}
+			}
+		}
+
 	}
 }
